d4ce5a8bab71291e670e90744cb0ccede9790ecb
kstoko02 <katyamy1008@gmail.com>
2022-10-18 21:51:57 +0800

    pick fruit game

---
+++ 1091421_hw6/Form1.Designer.cs
@@ -0,0 +1,174 @@
+﻿
+namespace _1091421_hw6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.restartToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.GripMargin = new System.Windows.Forms.Padding(2, 2, 0, 2);
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(678, 31);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.restartToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(55, 28);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // restartToolStripMenuItem
+            // 
+            this.restartToolStripMenuItem.Name = "restartToolStripMenuItem";
+            this.restartToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
+            this.restartToolStripMenuItem.Text = "Restart";
+            this.restartToolStripMenuItem.Click += new System.EventHandler(this.restartToolStripMenuItem_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(12, 634);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 18);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Remaining:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.Red;
+            this.label2.Location = new System.Drawing.Point(12, 675);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 18);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Receive:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.Color.Red;
+            this.label3.Location = new System.Drawing.Point(116, 634);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 18);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "120";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.ForeColor = System.Drawing.Color.Red;
+            this.label4.Location = new System.Drawing.Point(116, 675);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(16, 18);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "0";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.ForeColor = System.Drawing.Color.Red;
+            this.label5.Location = new System.Drawing.Point(183, 634);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(64, 18);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Seconds";
+            // 
+            // timer2
+            // 
+            this.timer2.Enabled = true;
+            this.timer2.Interval = 500;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(678, 725);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.DoubleBuffered = true;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem restartToolStripMenuItem;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Timer timer2;
+    }
+}
+
---
+++ 1091421_hw6/Form1.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Imaging;
+
+
+namespace _1091421_hw6
+{
+    public partial class Form1 : Form
+    {
+        Bitmap img,img1;
+        Bitmap[] picture;
+        Bitmap[] image;
+        int time;
+        int i;
+        ImageAttributes ia;
+        ColorMatrix cm;
+        Rectangle rect;
+        Point pos = new Point();
+        int n;
+        Random rd;
+        Point[] point;
+        int seconds;
+        int num;
+        float[][] cmArray1 = {
+                  new float[] {1, 0, 0, 0, 0},
+                  new float[] {0, 1, 0, 0, 0},
+                  new float[] {0, 0, 1, 0, 0},
+                  new float[] {0, 0, 0, 0.5f, 0},
+                  new float[] {0, 0, 0, 0, 1} };
+        public Form1()
+        {
+            InitializeComponent();
+            picture = new Bitmap[3];
+            picture[0] = Properties.Resources.Tulips;//圖
+            picture[1] = Properties.Resources.Hydrangeas;//圖
+            picture[2] = Properties.Resources.Penguins;//圖
+            img1 = Properties.Resources.Bowl;//碗
+            img = picture[0];
+            image = new Bitmap[3];
+            image[0] = Properties.Resources.StawBerry;
+            image[1] = Properties.Resources.Tomato;
+            image[2] = Properties.Resources.Banana;
+            i = 1;//圖變換
+            pos.Y = 175;
+            point = new Point[3];
+        }   
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            time = 0;
+            timer1.Start();
+            timer2.Start();
+            ia = new ImageAttributes();
+            cm = new ColorMatrix(cmArray1);
+            ia.SetColorMatrix(cm, ColorMatrixFlag.Default, ColorAdjustType.Bitmap);
+            rect = new Rectangle(0, 0, 450, 350);
+            rd = new Random();
+            for (int i = 0; i < 3; i++)
+            {
+                n = rd.Next(450 - image[i].Width);
+                point[i] = new Point(n, 20);
+            }
+            seconds = 120;
+            label3.Text = "120";
+            label4.Text = "0";
+            num = 0;
+        }
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.DrawImage(img, rect, 0, 0, img.Width, img.Height, GraphicsUnit.Pixel, ia);
+            for (int i = 0; i < 3; i++)
+                e.Graphics.DrawImage(image[i], point[i].X, point[i].Y, image[i].Width, image[i].Height);      
+            e.Graphics.TranslateTransform(pos.X, pos.Y);
+            e.Graphics.DrawImage(img1, pos.X, pos.Y,img1.Width, img1.Height);
+        }   
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Graphics g = this.CreateGraphics();
+            time++;
+            if (time % 10 == 0)
+            {
+                img = picture[i];
+                g.Clear(Color.White);
+                g.DrawImage(img, rect, 0, 0, img.Width, img.Height, GraphicsUnit.Pixel, ia);
+                i++;
+                if (i == 3)  
+                    i = 0;
+            }
+            label3.Text = seconds.ToString();
+            if (seconds == 0)
+            {
+                timer1.Stop();
+                timer2.Stop();
+            }
+            else
+                seconds--;
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            Graphics g = this.CreateGraphics();
+            for (int i = 0; i < 3; i++)
+            {
+                g.DrawImage(image[i], point[i].X, point[i].Y, image[i].Width, image[i].Height);
+                point[i].Y += 15;
+            }
+            if (point[0].Y > 320)
+            {
+                 if (point[0].X + picture[i].Width >= pos.X || point[0].X <= pos.X + img1.Width)
+                 {
+                     num++;
+                     label4.Text = num.ToString();
+                 }
+            }
+            if (point[1].Y > 320)
+            {
+                if (point[1].X + picture[i].Width >= pos.X || point[1].X <= pos.X + img1.Width)
+                {
+                    label4.Text = num.ToString();
+                }
+            }
+            if (point[2].Y > 320)
+            {
+                if (point[2].X + picture[2].Width >= pos.X || point[2].X <= pos.X + img1.Width)
+                {
+                    label4.Text = num.ToString();
+                }
+                for (int i = 0; i < 3; i++)
+                {
+                    n = rd.Next(450 - image[i].Width);
+                    point[i] = new Point(n, 20);
+                }
+            }
+        }
+
+        private void restartToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Form1_Load(sender, e);
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            Graphics g = this.CreateGraphics();
+            if (e.Location.X >= 1 && e.Location.X <= 270 - img1.Width)
+                pos = new Point(e.Location.X, pos.Y);
+            Invalidate();
+        }
+    }
+}
